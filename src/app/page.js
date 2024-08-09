e4d0dbcd0182be1f363f83912ae73dd81884d830
ramya <ramyas.aiml2022@citchennai.net>
2024-08-09 14:04:43 +0530

    changed layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
 import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
 import SearchSpace from "@/components/SearchSpace";
-import Section1 from "@/components/Section1";
 import { Playfair_Display } from "next/font/google";
 const pt = Playfair_Display({ weight:'700', subsets: ['latin'] });
 
@@ -10,8 +8,6 @@ const pt = Playfair_Display({ weight:'700', subsets: ['latin'] });
 export default function Home() {
   return (
     <main>
-    <Navbar />
-    <Section1 />
     <Hero />
     <div className="w-full font-extrabold tracking-wide p-6 flex justify-center items-center text-xl dec"><span className={pt.className}>WELCOME TO MULTI-LINGUAL CONVERSATIONAL ASSISTANT</span></div>
     <SearchSpace />
