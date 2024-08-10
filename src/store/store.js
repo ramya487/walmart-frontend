684597f9a1b42836b6f2f93d4b97afd1d5da40c4
ramya <ramyas.aiml2022@citchennai.net>
2024-08-10 11:24:45 +0530

    added state persistence to zustand

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,14 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
-const useProduct = create((set) => (
+const useProduct = create(persist((set) => (
     {
         product: [],
         setProduct: (arry) => set(state => ({product: arry}))
     }
-))
+),
+{
+    name: "ecommerce-state"
+}))
 
 export default useProduct
